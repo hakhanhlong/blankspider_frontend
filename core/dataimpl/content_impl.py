377d7f689c4396d38fc5f2e00c237822d463bb7b
longhk <hakhanhlong@gmail.com>
2017-03-23 18:22:19 -0700

    commit frontend

--- core/dataimpl/content_impl.py
+++ core/dataimpl/content_impl.py
@@ -2,7 +2,7 @@ from ..database.content import CONTENTS
 from datetime import datetime
 
 def get_bytagid(sid):
-    t = CONTENTS.objects(tag_id = str(sid)).no_cache()[:50]
+    t = CONTENTS.objects(tag_id = str(sid)).order_by('-created_at').no_cache()[:100]
     #t = CONTENTS.objects()[:50]
     return t
 

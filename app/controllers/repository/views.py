2d8fef3d724a248c3bfcf13ee32df37b47a4106b
hieuh <hieuhtgc00378@gmail.com>
2017-05-24 17:44:27 -0700

    front end for lcbc

--- app/controllers/repository/views.py
+++ app/controllers/repository/views.py
@@ -2,6 +2,7 @@
 from flask import render_template, jsonify, flash
 from flask.ext.login import login_required
 
+from core.api.services.content_service import ContentService
 from . import repository
 from core.dataimpl import tag_impl
 from core.dataimpl import content_impl
@@ -14,9 +15,49 @@ from datetime import datetime
 
 from core.api.request_helpers import RequestHelpers
 from core.api.request_url import RequestURL
+from core.control.pagination import Pagination
+
+
+def get_data_from_service_filter_by_default(page=0):
+    content_service = ContentService()
+    content = content_service.list_by_default(int(page), 50)
+    count_items = content['response']['numFound']
+    pagination = Pagination(int(page), 50, count_items)
+    data_master = []
+    for item in content['response']['docs']:
+        data_master.append(item)
+    aDict = {}
+    aDict['items'] = data_master
+    aDict['pagingnation'] = pagination;
+    return aDict;
+
+
+def get_data_from_service_filter_by_timing(sid, ptimingid, page=0):
+    content_service = ContentService()
+    content = content_service.filter_by_timing(sid, ptimingid, int(page), 50)
+    count_items = content['response']['numFound']
+    pagination = Pagination(int(page), 50, count_items)
+    data_master = []
+    for item in content['response']['docs']:
+        data_master.append(item)
+    aDict = {}
+    aDict['items'] = data_master
+    aDict['pagingnation'] = pagination;
+    return aDict
+
+
+@repository.route('/filter-by-timing/<sid>/<ptimingid>/', methods=['GET'])
+@repository.route('/filter-by-timing/<sid>/<ptimingid>/<page>', methods=['GET'])
+def content_filter_by_timing(sid, ptimingid, page=0):
+    aDict = get_data_from_service_filter_by_timing(sid, ptimingid, page)
+    print("items = "+str(len(aDict['items'])))
+    return render_template('repository/datatable-middle-content.html', contents=aDict['items'], pagination=aDict['pagingnation'],
+                           params={'sid': sid, 'ptimingid': ptimingid})
+
 
 @repository.route('/', methods=['GET'])
-def index():
+@repository.route('/<page>', methods=['GET'])
+def index(page=0):
     sources = source_impl.get_all_active(True)
     _request = RequestHelpers()
     _requestURL = RequestURL()
@@ -29,21 +70,24 @@ def index():
             timings = _request.post_json().json()
             for timing in timings:
                 try:
-                    timing['published_at'] = datetime.strptime(timing['published_at'].replace('T00:00:00.000Z', ''),'%Y-%m-%d')
+                    timing['published_at'] = datetime.strptime(timing['published_at'].replace('T00:00:00.000Z', ''),
+                                                               '%Y-%m-%d')
                 except:
                     break
             x.timings = timings
-        # ------------------------------------------------------------------------------------------------------------------
+            # ------------------------------------------------------------------------------------------------------------------
+        aDict = get_data_from_service_filter_by_default(page)
     except Exception as ex:
         flash('ERROR:' + ex, 'danger')
 
+    return render_template('repository/index2.html', sources=sources, contents=aDict['items'],
+                           pagination=aDict['pagingnation'])
 
-    return render_template('repository/index.html', sources = sources)
 
 @repository.route('/detail/<cid>', methods=['GET'])
 def detail(cid):
     cont = content_impl.get_byid(cid)
-    #n_dict = json.loads(cont.data)
+    # n_dict = json.loads(cont.data)
     data_master = []
 
     try:
@@ -53,13 +97,13 @@ def detail(cid):
     except Exception as ex:
         flash('ERROR:' + ex.message, 'danger')
 
+    return render_template('repository/detail.html', data=data_master, link_href=cont.href, contentid=cid)
 
-    return render_template('repository/detail.html', data=data_master, link_href = cont.href, contentid=cid)
 
 @repository.route('/detail-html/<cid>/<idx>', methods=['GET'])
 def detail_html(cid, idx):
     cont = content_impl.get_byid(cid)
-    #n_dict = json.loads(cont.data)
+    # n_dict = json.loads(cont.data)
     data_master = []
     try:
         for k, v in cont.data[int(idx)].items():
@@ -67,7 +111,8 @@ def detail_html(cid, idx):
     except Exception as ex:
         flash('ERROR:' + ex.message, 'danger')
 
-    #cont.data = None
+    # cont.data = None
 
-    #return data_master[0]['value']['html_data'].replace("document.domain", "")
-    return render_template('repository/detail_html.html', data=data_master[0]['value']['html_data'].replace("document.domain", ""))
+    # return data_master[0]['value']['html_data'].replace("document.domain", "")
+    return render_template('repository/detail_html.html',
+                           data=data_master[0]['value']['html_data'].replace("document.domain", ""))
--- app/static/scripts/frontend.js
+++ app/static/scripts/frontend.js
@@ -1,54 +1,69 @@
-$(document).ready(function(){
+$(document).ready(function () {
 
-    $('#select-baodientu-repository').change(function(){
+    $('#select-baodientu-repository').change(function () {
         $('#select-tag option').remove();
         $.ajax('/ajax/tag/get_by_source/' + this.value, {
-              success: function(data) {
+            success: function (data) {
 
-                $.each(data, function(index, item){
-                    $('#select-tag').append('<option value="'+ item._id +'">' + item.name  + ' ('+item.count+')'+ '</option>');
+                $.each(data, function (index, item) {
+                    $('#select-tag').append('<option value="' + item._id + '">' + item.name + ' (' + item.count + ')' + '</option>');
                 });
 
                 $('#select-tag').selectpicker('refresh');
-              },
-              error: function() {
-                 console.log('ERROR: GET TAG BY SOURCE');
-              }
-           });
+            },
+            error: function () {
+                console.log('ERROR: GET TAG BY SOURCE');
+            }
+        });
 
     }).change();
 
-    $('.lv-body').on('click', '#filter-timing', function(){
+    $('.lv-body').on('click', '#filter-timing', function () {
         var sourceid = $(this).attr('sourceid');
         var timingid = $(this).attr('timingid');
         var page = $(this).attr('page');
 
 
-          $.ajax('/ajax/content/filter_by_ptiming/' + sourceid + '/' + timingid + '/' + page, {
-              success: function(data) {
+        $.ajax('/ajax/content/filter_by_ptiming/' + sourceid + '/' + timingid + '/' + page, {
+            success: function (data) {
 
                 $('.table-responsive').html(data);
 
-              },
-              error: function() {
-                 console.log('ERROR: GET CONTENT');
-              }
-           });
+            },
+            error: function () {
+                console.log('ERROR: GET CONTENT');
+            }
+        });
     });
 
-    $('#btn-search').click(function(){
+    $('.menu-tree-link').on('click', function () {
+        var sourceid = $(this).attr('sourceid');
+        var timingid = $(this).attr('timingid');
+        var page = $(this).attr('page');
+
+
+        $.ajax('/repository/filter-by-timing/' + sourceid + '/' + timingid + '/' + page, {
+            success: function (data) {
+                  $('.midle-content').html(data);
+
+            },
+            error: function () {
+                console.log('ERROR: GET CONTENT');
+            }
+        });
+    });
+
+    $('#btn-search').click(function () {
         var source = $('#select-baodientu-repository').val();
         var tag = $('#select-tag').val();
         var kw = $('#txt-search').val();
         var published = $('#txt-published-date').val();
 
-        if(!kw)
-        {
+        if (!kw) {
             kw = "*";
         }
 
-        if(!published)
-        {
+        if (!published) {
             published = "*";
         }
 
@@ -56,100 +71,98 @@ $(document).ready(function(){
         published = published.split('/').join(newchar);
 
         var url = '/ajax/content/search/' + source + '/' + tag + '/' + published + '/' + kw + '/1';
-         $.ajax(encodeURI(url), {
-          success: function(data) {
+        $.ajax(encodeURI(url), {
+            success: function (data) {
 
-            $('.table-responsive').html(data);
+                $('.table-responsive').html(data);
 
-          },
-          error: function() {
-             console.log('ERROR: GET CONTENT');
-          }
-       });
+            },
+            error: function () {
+                console.log('ERROR: GET CONTENT');
+            }
+        });
     });
 
 });
 
-function init_repository(){
+function init_repository() {
 
-      $.ajax('/ajax/content/list_by_default', {
-          success: function(data) {
+    $.ajax('/ajax/content/list_by_default', {
+        success: function (data) {
 
             $('.table-responsive').html(data);
 
-          },
-          error: function() {
-             console.log('ERROR: GET CONTENT');
-          }
-       });
+        },
+        error: function () {
+            console.log('ERROR: GET CONTENT');
+        }
+    });
 }
 
-function pagination_ajax(obj){
+function pagination_ajax(obj) {
 
     var sourceid = $(obj).attr('sourceid');
     var timingid = $(obj).attr('timingid');
     var page = $(obj).attr('page');
 
-        $('.table-responsive').html('Xin đợi trong giây lát ...');
-      $.ajax('/ajax/content/filter_by_ptiming/' + sourceid + '/' + timingid + '/' + page, {
-          success: function(data) {
+    $('.table-responsive').html('Xin đợi trong giây lát ...');
+    $.ajax('/ajax/content/filter_by_ptiming/' + sourceid + '/' + timingid + '/' + page, {
+        success: function (data) {
 
             $('.table-responsive').html(data);
 
-          },
-          error: function() {
-             console.log('ERROR: GET CONTENT');
-          }
-       });
+        },
+        error: function () {
+            console.log('ERROR: GET CONTENT');
+        }
+    });
 }
 
-function pagination_ajax_content_default(obj){
+function pagination_ajax_content_default(obj) {
 
 
     var page = $(obj).attr('page');
 
     $('.table-responsive').html('Xin đợi trong giây lát ...');
-      $.ajax('/ajax/content/list_by_default/' + page, {
-          success: function(data) {
+    $.ajax('/ajax/content/list_by_default/' + page, {
+        success: function (data) {
 
             $('.table-responsive').html(data);
 
-          },
-          error: function() {
-             console.log('ERROR: GET CONTENT');
-          }
-       });
+        },
+        error: function () {
+            console.log('ERROR: GET CONTENT');
+        }
+    });
 }
 
-function pagination_ajax_search_content(obj){
-        var page = $(obj).attr('page');
-        var source = $('#select-baodientu-repository').val();
-        var tag = $('#select-tag').val();
-        var kw = $('#txt-search').val();
-        var published = $('#txt-published-date').val();
+function pagination_ajax_search_content(obj) {
+    var page = $(obj).attr('page');
+    var source = $('#select-baodientu-repository').val();
+    var tag = $('#select-tag').val();
+    var kw = $('#txt-search').val();
+    var published = $('#txt-published-date').val();
 
-        if(!kw)
-        {
-            kw = "*";
-        }
+    if (!kw) {
+        kw = "*";
+    }
 
-        if(!published)
-        {
-            published = "*";
-        }
+    if (!published) {
+        published = "*";
+    }
 
-        var newchar = '-'
-        published = published.split('/').join(newchar);
+    var newchar = '-'
+    published = published.split('/').join(newchar);
 
-        var url = '/ajax/content/search/' + source + '/' + tag + '/' + published + '/' + kw + '/' + page;
-         $.ajax(encodeURI(url), {
-          success: function(data) {
+    var url = '/ajax/content/search/' + source + '/' + tag + '/' + published + '/' + kw + '/' + page;
+    $.ajax(encodeURI(url), {
+        success: function (data) {
 
             $('.table-responsive').html(data);
 
-          },
-          error: function() {
-             console.log('ERROR: GET CONTENT');
-          }
-       });
+        },
+        error: function () {
+            console.log('ERROR: GET CONTENT');
+        }
+    });
 }

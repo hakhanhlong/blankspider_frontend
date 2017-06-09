429d2658e824958003b0fe4d9a6b0e9313b430cf
hieuh <hieuhtgc00378@gmail.com>
2017-06-09 10:56:20 +0700

    update UI for mobile

--- app/static/scripts/frontend.js
+++ app/static/scripts/frontend.js
@@ -4,20 +4,67 @@ var PAGE_FILTER_BY_TIMING = 1;
 var PAGE_SEARCH = 3;
 var scroll_position = 0;
 var POSITION_TO_SCROLL_TO = 204;
+var isLeftMenuTreeDisplay = false;
+var MOBILE_WIDTH = 840;
+var MOBILE_SCROLL_POSITION = 350;
+var mobile_check = false;
+var tablet_check = false;
+var is_navigationbar_opened = false;
 $(document).ready(function () {
+    window.mobilecheck = function () {
+        (function (a) {
+            if (/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(a) || /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0, 4))) mobile_check = true;
+        })(navigator.userAgent || navigator.vendor || window.opera);
+        return mobile_check;
+    };
+    window.mobileAndTabletcheck = function () {
+        (function (a) {
+            if (/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino|android|ipad|playbook|silk/i.test(a) || /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0, 4))) tablet_check = true;
+        })(navigator.userAgent || navigator.vendor || window.opera);
+        return tablet_check;
+    };
+    mobile_check = window.mobilecheck;
+    tablet_check = window.mobileAndTabletcheck;
+    if (mobile_check == true || tablet_check == true) {
+        MOBILE_WIDTH = 840;
+    } else {
+        MOBILE_WIDTH = 823;
+    }
+    resize_for_mobile();
+    $('#btn_collapsed').on('click', function () {
+        if (is_navigationbar_opened == false) {
+            $('#defaultNavbar1').css({background: '#134e70'});
+            is_navigationbar_opened = true;
+        } else {
+            $('#defaultNavbar1').css({background: 'transparent'});
+            is_navigationbar_opened = false;
+        }
+    });
+    $('#btn_slide_right').on('click', function () {
+        if (isLeftMenuTreeDisplay == false) {
+            open_left_menu_tree();
+        } else {
+            close_left_menu_tree();
+        }
+    });
     $('.open_popup').on('click', function () {
         var index = $(this).attr('index');
         resize_iframe(index);
     });
     reset_leftMenuTree_height();
     $(window).resize(function () {
-        var windowHeight = $(window).height();
-        if (scroll_position >= POSITION_TO_SCROLL_TO) {
-            $('#left-menu-tree').css({height: windowHeight});
-        } else {
-            reset_leftMenuTree_height();
+        $('#txt-search').val($(window).width());
+        if ($(window).width() > MOBILE_WIDTH) {
+            var windowHeight = $(window).height();
+            if (scroll_position >= POSITION_TO_SCROLL_TO) {
+                $('#left-menu-tree').css({height: windowHeight});
+            } else {
+                reset_leftMenuTree_height();
+            }
         }
+        resize_for_mobile();
     });
+
     $('#left-menu-tree').bind('mousewheel DOMMouseScroll', function (e) {
         var scrollTo = null;
 
@@ -33,27 +80,37 @@ $(document).ready(function () {
             $(this).scrollTop(scrollTo + $(this).scrollTop());
         }
     });
-
     var windowHeight = $(window).height() - $('#footer').height();
     $(window).scroll(function () {
-        scroll_position = $(window).scrollTop();
-        if (scroll_position >= POSITION_TO_SCROLL_TO) {
+        if ($(window).width() > MOBILE_WIDTH) {
+            scroll_position = $(window).scrollTop();
+            if (scroll_position >= POSITION_TO_SCROLL_TO) {
 
-            var height = 0;
-            if (isFooterViewOnScreen()) {
-                height = $(window).height() - $('#footer').height() - 5;
+                var height = 0;
+                if (isFooterViewOnScreen()) {
+                    height = $(window).height() - $('#footer').height() - 5;
+                } else {
+                    height = $(window).height();
+                }
+                $('#left-menu-tree').css({
+                    position: 'fixed',
+                    top: 0,
+                    z_index: -1,
+                    height: height
+                });
             } else {
-                height = $(window).height();
+                $('#left-menu-tree').css({position: 'relative'});
+                reset_leftMenuTree_height();
             }
-            $('#left-menu-tree').css({
-                position: 'fixed',
-                top: 0,
-                z_index: -1,
-                height: height
-            });
         } else {
-            $('#left-menu-tree').css({position: 'relative'});
-            reset_leftMenuTree_height();
+            if ($(window).scrollTop() >= MOBILE_SCROLL_POSITION) {
+                $('#btn_slide_right').css({display: 'block'});
+            } else {
+                $('#btn_slide_right').css({display: 'none'});
+                if (isLeftMenuTreeDisplay) {
+                    close_left_menu_tree();
+                }
+            }
         }
     });
     $('#select-baodientu-repository').change(function () {
@@ -420,3 +477,60 @@ function convertFormatDate(date) {
     date = day + "/" + month + "/" + year;
     return date;
 }
+
+function resize_for_mobile() {
+    var horizontalMenubar_height = $('#horizontal-menubar').height();
+    if ($(window).width() <= MOBILE_WIDTH) {
+        $('#admin-area').css({height: horizontalMenubar_height});
+        $('#btn-search').appendTo('#horizontal-menubar-date');
+        $('#btn-search').css("margin-top", 2 + "px");
+        $('#btn-search').css("margin-bottom", 1 + "px");
+        var leftMenuTreeWidth = $('#left-menu-tree').width();
+        if (isLeftMenuTreeDisplay == false) {
+            var h = $(window).height();
+            $('#left-menu-tree').css({position: 'fixed', left: -leftMenuTreeWidth, height: h});
+            var slide_right_top = ($('#left-menu-tree').height / 2) - ($('#btn_slide_right').height() / 2);
+            $('#btn_slide_right').css({top: slide_right_top});
+        }
+        $('.midle-content').css({float: 'left', width: 100 + '%'});
+    } else {
+        $('#admin-area').css({height: horizontalMenubar_height});
+        $('#btn-search').appendTo('#horizontal-menubar-column3');
+        $('#btn-search').css("margin-top", 20 + "%");
+        $('.midle-content').css({float: 'right', width: 77 + '%'});
+        if (scroll_position >= POSITION_TO_SCROLL_TO) {
+            $('#left-menu-tree').css({position: 'absolute', left: 0});
+        } else {
+            $('#left-menu-tree').css({position: 'relative', left: 0});
+        }
+    }
+}
+
+function open_left_menu_tree() {
+    $('#open_menutree').css({display: 'none'});
+    $('#close_menutree').css({display: 'block'});
+    var destination = $('#left-menu-tree').width();
+    $('#left-menu-tree').animate({
+        left: "+=" + destination + "px",
+    }, 1000);
+    $('#btn_slide_right').animate({
+        left: "+=" + destination + "px",
+    }, 1000);
+    isLeftMenuTreeDisplay = true;
+    var h = $(window).height();
+    $('#left-menu-tree').css({height: h, top: 5});
+}
+
+function close_left_menu_tree() {
+    $('#open_menutree').css({display: 'block'});
+    $('#close_menutree').css({display: 'none'});
+    var destination = $('#left-menu-tree').width();
+    $('#left-menu-tree').animate({
+        left: "-=" + destination + "px",
+    }, 1000);
+    $('#btn_slide_right').animate({
+        left: "-=" + destination + "px",
+    }, 1000);
+    isLeftMenuTreeDisplay = false;
+}
+

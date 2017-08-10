4441f4aa3edc92332ab6d3209ebc35ca6530653f
hieuhtgc00378 <hieuhtgc00378@gmail.com>
2017-08-10 08:41:34 +0700

    Revert: cannot open and close left menu tree

--- core/api/services/content_service.py
+++ core/api/services/content_service.py
@@ -12,7 +12,6 @@ class ContentService:
 
     def filter_by_timing(self, source_id = '', publishtiming_id='', pageindex=0, pagesize=50):
         self.requestHelpers.url = self.request_URL.CONTENT_URL_FILTER_BY_TIMING % (publishtiming_id, source_id, pagesize, pageindex)
-        print("url = "+self.requestHelpers.url)
         #self.requestHelpers.params = {'source_id': source_id, 'publishtiming_id':publishtiming_id,
         #                              'pageindex':pageindex, 'pagesize':pagesize}
 
@@ -24,8 +23,7 @@ class ContentService:
         return self.requestHelpers.get().json()
 
 
-    def search(self,source_id='', tagid='', published_at='', keyword='', pageindex=0, pagesize=50):
-        pageindex = pageindex-1;
+    def search(self,source_id='', tagid='', published_at='', keyword='', pageindex=1, pagesize=50):
         if published_at is not '*':
             published_at = parse(published_at)
             published_at = published_at.strftime('"%Y-%m-%dT00:00:00Z"')
